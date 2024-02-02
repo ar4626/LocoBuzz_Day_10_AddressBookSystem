48b708634b426cdbc0d770072422fdc8b9b95c6e
Ankit Raj <ankitsingh17072001@gmail.com>
2024-02-02 11:36:21 +0530

    [Ankit] Update : Minor update in file structure

--- Contact.cs
+++ Contact.cs
@@ -6,7 +6,50 @@ using System.Threading.Tasks;
 
 namespace Address_Book_System
 {
-    internal class Contact
+    class Contact
     {
+        private string fname;
+        private string lname;
+        private string address;
+        private string city;
+        private string state;
+        private string phonenumber;
+        private string email;
+
+        public string Fname
+        {
+            get { return fname; }
+            set { fname = value; }
+        }
+        public string Lname
+        {
+            get { return lname; }
+            set { lname = value; }
+        }
+        public string Address
+        {
+            get { return address; }
+            set { address = value; }
+        }
+        public string City
+        {
+            get { return city; }
+            set { city = value; }
+        }
+        public string State
+        {
+            get { return state; }
+            set { state = value; }
+        }
+        public string Phonenumber
+        {
+            get { return phonenumber; }
+            set { phonenumber = value; }
+        }
+        public string Email
+        {
+            get { return email; }
+            set { email = value; }
+        }
     }
 }
--- User.cs
+++ User.cs
@@ -6,7 +6,35 @@ using System.Threading.Tasks;
 
 namespace Address_Book_System
 {
-    internal class User
+    class User
     {
+        private Dictionary<string, AddressBook> users;
+
+        public User()
+        {
+            users = new Dictionary<string, AddressBook>();
+        }
+
+        public void addUser(string name)
+        {
+            AddressBook book = new AddressBook();
+            users.Add(name, book);
+        }
+
+        public void deleteUser(string name)
+        {
+            users.Remove(name);
+        }
+
+        public AddressBook getAddressBook(string name)
+        {
+            return users[name];
+        }
+
+        public Dictionary<string, AddressBook> GetUser()
+        {
+            return users;
+        }
+
     }
 }
